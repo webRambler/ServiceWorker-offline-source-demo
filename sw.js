7168641e5fb7bf6477186624f97ad4374adc4105
webRambler <talent1203@163.com>
2019-10-09 15:44:05 +0800

    feat：增加增量的缓存新的请求

--- sw.js
+++ sw.js
@@ -27,11 +27,19 @@ self.addEventListener('fetch', event => {
         return cache
       } else {
         console.log('此处的资源需要向网络进行请求', req);
-        // 可以使用fetch进行请求
-        // fetch(req.url).then(data => {
-        //   ......
-        // })
-      }
+        // 如果我们想要增量的缓存新的请求，我们可以通过处理fetch请求的response并且添加它们到缓存中来实现(如下代码所示)
+        var fetchRequest = req.clone();
+        return fetch(fetchRequest).then(response=> {
+            if (!response || response.status !== 200 || !response.headers.get('Content-type').match(/img/)) {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(SW_CACHE_NAME)
+              .then(function (cache) {
+                cache.put(req, responseToCache);
+              })
+            })
+          }
     })
   )
 })
