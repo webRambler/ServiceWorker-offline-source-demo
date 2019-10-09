91ee59b6f12085d28e34b9000250489ed14e90df
webRambler <talent1203@163.com>
2019-10-09 15:47:00 +0800

    feat：添加相关注释

--- sw.js
+++ sw.js
@@ -26,14 +26,19 @@ self.addEventListener('fetch', event => {
       if (cache) {
         return cache
       } else {
-        console.log('此处的资源需要向网络进行请求', req);
         // 如果我们想要增量的缓存新的请求，我们可以通过处理fetch请求的response并且添加它们到缓存中来实现(如下代码所示)
+
+        console.log('此处的资源需要向网络进行请求', req);
+        // 克隆请求。因为请求是一个“stream”，只能用一次。但我们需要用两次，一次用来缓存，一次给浏览器抓取内容，所以需要克隆
         var fetchRequest = req.clone();
         return fetch(fetchRequest).then(response=> {
+            // 检查是否为有效的响应。basic表示同源响应，也就是说，这意味着，对第三方资产的请求不会添加到缓存。
             if (!response || response.status !== 200 || !response.headers.get('Content-type').match(/img/)) {
               return response;
             }
+            // 同request，response是一个“stream”，只能用一次，但我们需要用两次，一次用来缓存一个返回给浏览器，所以需要克隆。
             var responseToCache = response.clone();
+            // 缓存新请求
             caches.open(SW_CACHE_NAME)
               .then(function (cache) {
                 cache.put(req, responseToCache);
